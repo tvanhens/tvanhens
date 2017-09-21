3b3d62d952804ada5663b627f9e42aae5567ac7d
Tyler van Hensbergen <tyler@farmlogs.com>
2017-09-20 19:21:03 -0600

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
   :lein-release {:deploy-via :clojars}
-  :plugins [[lein-release "1.0.5"]])
+  :plugins [[lein-release "1.0.9"]])
