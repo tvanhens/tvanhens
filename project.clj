85c34d18cc98280308764265252adee5ac7009d4
Tyler van Hensbergen <tvanhens@gmail.com>
2017-09-21 03:08:50 +0000

    lein-release plugin: preparing 0.2.8 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tvanhens "0.2.8-SNAPSHOT"
+(defproject tvanhens "0.2.8"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
