8861a77cf6ee33151d9c86a910609a567e80d174
Tyler van Hensbergen <tyler@farmlogs.com>
2017-09-20 20:19:09 -0600

    add docs add docs [skip ci]

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
   :lein-release {:deploy-via :clojars}
-  :plugins [[lein-release "1.0.9"]]
+  :plugins [[lein-codox "0.10.3"]
+            [lein-release "1.0.9"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version"
                    "leiningen.release/bump-version" "release"]
@@ -15,6 +16,7 @@
                   ["file-replace" "README.md" "\\[api \"" "\"]" "version"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]]
+  :codox {:output-path "docs"}
   :deploy-repositories
   {"clojars" {:url           "https://clojars.org/repo"
               :sign-releases false
