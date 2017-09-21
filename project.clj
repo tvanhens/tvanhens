b02dd3c948899149da371bfef1ef2eb5f813c53f
Tyler van Hensbergen <tyler@farmlogs.com>
2017-09-20 21:07:45 -0600

    try unison

--- project.clj
+++ project.clj
@@ -6,15 +6,21 @@
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
   :lein-release {:deploy-via :clojars}
   :plugins [[lein-codox "0.10.3"]
-            [lein-release "1.0.9"]]
+            [lein-release "1.0.9"]
+            [lonocloud/lein-unison "0.1.13"]]
   :release-tasks
   [["vcs" "assert-committed"]
    ["change" "version" "leiningen.release/bump-version" "release"]
    ["vcs" "commit"]
    ["vcs" "tag" "--no-sign"]
    ["deploy"]
+   ["unison" "release-projects"]
    ["change" "version" "leiningen.release/bump-version"]
    ["vcs" "commit"]]
+  :unison
+  [{:git            "git@github.com:tvanhens/track.git"
+    :release-script "script/release.sh"
+    :branch         "master"}]
   :codox {:output-path "docs"}
   :deploy-repositories
   {"clojars" {:url           "https://clojars.org/repo"
