d74bfcff5182e7e28a3117eace8dee8df465c805
Tyler van Hensbergen <tyler@farmlogs.com>
2017-09-20 21:42:14 -0600

    bump broken version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tvanhens "0.2.10-SNAPSHOT"
+(defproject tvanhens "0.2.11-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
