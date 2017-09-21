e3ebb1072725080c803176ee49d3cb88743ef66a
Tyler van Hensbergen <tvanhens@gmail.com>
2017-09-21 03:43:07 +0000

    Version 0.2.12-SNAPSHOT [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tvanhens "0.2.11"
+(defproject tvanhens "0.2.12-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
