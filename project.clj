faee18d70af24909e76dfd827a7b30dc0aa520a2
Tyler van Hensbergen <tyler@farmlogs.com>
2017-09-20 19:25:12 -0600

    lein-release plugin: bumped version from 0.2.0 to 0.2.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tvanhens "0.2.0"
+(defproject tvanhens "0.2.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
