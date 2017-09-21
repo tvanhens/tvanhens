61ee58d2f49ff2440bce3fadbae59961d0e9b6d7
Tyler van Hensbergen <tyler@farmlogs.com>
2017-09-20 20:04:03 -0600

    bump version for deploy key

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tvanhens "0.2.3-SNAPSHOT"
+(defproject tvanhens "0.2.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
