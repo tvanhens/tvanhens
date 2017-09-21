e979485dd63a7fcd82c0ee55d1df1a0ac4abaa98
Tyler van Hensbergen <tvanhens@gmail.com>
2017-09-21 03:09:09 +0000

    lein-release plugin: bumped version from 0.2.8 to 0.2.9-SNAPSHOT for next development cycle [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tvanhens "0.2.8"
+(defproject tvanhens "0.2.9-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
