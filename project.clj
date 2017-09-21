377539da1d5f6fbb93afd8fb0ef8490314224778
Tyler van Hensbergen <tyler@farmlogs.com>
2017-09-20 19:29:59 -0600

    lein-release plugin: preparing 0.2.1 release

--- project.clj
+++ project.clj
@@ -1,8 +1,17 @@
-(defproject tvanhens "0.2.1-SNAPSHOT"
+(defproject tvanhens "0.2.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
   :lein-release {:deploy-via :clojars}
-  :plugins [[lein-release "1.0.9"]])
+  :plugins [[lein-release "1.0.9"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["file-replace" "README.md" "\\[api \"" "\"]" "version"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]])
